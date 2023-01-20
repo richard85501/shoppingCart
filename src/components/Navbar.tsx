655a70e3a1898a6118f64f8b7d1f9b0c62cc718f
richard85501 <liuchang85501@gmail.com>
2023-01-20 21:17:56 +0800

    create master

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import { Navbar as NavbarBs, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { MdShoppingCart } from "react-icons/md";
+
+export function Navbar() {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          variant="outline-primary"
+          className="rounded-circle"
+          style={{ position: "relative", width: "3rem", height: "3rem" }}
+        >
+          <MdShoppingCart style={{ fontSize: "25px" }} />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%,25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavbarBs>
+  );
+}
---
+++ src/pages/Store.tsx
@@ -0,0 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import storeItem from "../data/item.json";
+import { StoreItem } from "../components/StoreItem";
+
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItem.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
